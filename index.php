0df8c85c1863aebebb88d582e11419a388b26865
jihan <jihanislam007@gmail.com>
2021-01-16 13:03:12 +0600

    with droup down list... v2

--- index.php
+++ index.php
@@ -95,7 +95,14 @@
                   <tbody>
                     <tr>
                       <th scope="row">1</th>
-                      <td><input class="form-control" type="text" name="textProductname[]" required=""></td>
+                      <td >                         
+                          <select class="form-control" name="textProductname[]">
+                            <option value="">Select Product Name ....</option>
+                            <option value="Meat More">Meat More</option>
+                            <option value="By pro 25kg">By pro 25kg</option>
+                          </select>
+                      </td>
+
                       <td><input class="form-control" type="text" name="textQuantity[]" required=""></td>
                       <td><input class="form-control" type="text" name="textBag[]" required=""></td>
                       <td><input class="form-control" type="text" name="textPrice[]" required=""></td>
@@ -162,7 +169,10 @@
 <script type="text/javascript">
         $(document).ready(function(){
 
-          var table_raw = '<tr><th scope="row">1</th><td><input class="form-control" type="text" name="textProductname[]" required=""></td><td><input class="form-control" type="text" name="textQuantity[]" required=""></td><td><input class="form-control" type="text" name="textBag[]" required=""></td><td><input class="form-control" type="text" name="textPrice[]" required=""></td><td><input class="btn btn-danger" type="button" name="remove" id="remove" value="Remove"></td></tr>';
+          // var table_raw = '<tr><th scope="row">1</th><td><input class="form-control" type="text" name="textProductname[]" required=""></td><td><input class="form-control" type="text" name="textQuantity[]" required=""></td><td><input class="form-control" type="text" name="textBag[]" required=""></td><td><input class="form-control" type="text" name="textPrice[]" required=""></td><td><input class="btn btn-danger" type="button" name="remove" id="remove" value="Remove"></td></tr>';
+
+
+          var table_raw = '<tr><th scope="row">1</th><td ><select class="form-control" name="textProductname[]"><option value="">Select Product Name ....</option><option value="Meat More">Meat More</option><option value="By pro 25kg">By pro 25kg</option></select></td><td><input class="form-control" type="text" name="textQuantity[]" required=""></td><td><input class="form-control" type="text" name="textBag[]" required=""></td><td><input class="form-control" type="text" name="textPrice[]" required=""></td><td><input class="btn btn-warning" type="button" name="add" id="add" value="ADD"></td></tr>';
 
           var max = 6;
           var x = 1;
@@ -188,3 +198,4 @@
 
 <!-- tutorial link -->
 <!-- https://www.youtube.com/watch?v=7efeIJ7oFTc&ab_channel=ShajedulIslamShawon -->
+<!--dropdown----------- https://www.youtube.com/watch?v=XVRA4XyN-m0 -->
